5c2cef9811e2cdb20dc88c7cfad767811d21b93f
Feindad <Feindad@gmail.com>
2023-01-07 19:01:12 -0500

    Update PG-read_write_file.py

--- PG-read_write_file.py
+++ PG-read_write_file.py
@@ -1,2 +1,15 @@
 #EX8 Pyton Gently Read Write File
 
+def writeToFile(file,text):
+    pass
+
+def appendToFile(file,text):
+    pass
+
+def readFromFile(file):
+    pass
+
+
+assert readFromFile('greet.txt') == 'Hello!\nGoodbye!\n'
+
+
