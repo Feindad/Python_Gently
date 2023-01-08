71cb46e7563398db13e9979beeca34b7acb98881
Feindad <Feindad@gmail.com>
2023-01-08 09:57:11 -0500

    Create PG10-find_and_replace.py

---
+++ PG10-find_and_replace.py
@@ -0,0 +1,18 @@
+### Python Gently Ex-10 Find and Replace 
+
+def findAndReplace(text,oldText,newText):
+    text = text.replace(oldText,newText)
+    return text
+
+
+
+assert findAndReplace('The fox', 'fox', 'dog') == 'The dog'
+
+assert findAndReplace('fox', 'fox', 'dog') == 'dog'
+
+assert findAndReplace('Firefox', 'fox', 'dog') == 'Firedog'
+
+assert findAndReplace('foxfox', 'fox', 'dog') == 'dogdog'
+
+assert findAndReplace('The Fox and fox.', 'fox', 'dog') == 'The Fox and dog.'
+
