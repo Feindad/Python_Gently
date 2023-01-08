784a67f8f35825f514d99621f3a0f139e54c90b3
Feindad <Feindad@gmail.com>
2023-01-07 19:08:19 -0500

    finished project
    remove pass statements

--- PG-read_write_file.py
+++ PG-read_write_file.py
@@ -1,15 +1,21 @@
 #EX8 Pyton Gently Read Write File
 
 def writeToFile(file,text):
-    pass
+    f = open(file, "w")
+    f.write(text)
+    f.close()
 
 def appendToFile(file,text):
-    pass
+    f = open(file, "a")
+    f.write(text)
+    f.close()
 
 def readFromFile(file):
-    pass
+    f = open(file, "r")
+    return(f.read())
 
-
+writeToFile('greet.txt', 'Hello!\n')
+appendToFile('greet.txt', 'Goodbye!\n')
 assert readFromFile('greet.txt') == 'Hello!\nGoodbye!\n'
 
 
