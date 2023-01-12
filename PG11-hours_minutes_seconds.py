91468c1f3212a3c858bcb224ec8cfe1cd01f214c
Feindad <Feindad@gmail.com>
2023-01-11 22:06:59 -0500

    Update PG11-hours_minutes_seconds.py

--- PG11-hours_minutes_seconds.py
+++ PG11-hours_minutes_seconds.py
@@ -1,5 +1,28 @@
 #Python Gently Ex-11 Hours, Minutes, Seconds
 
+def getHoursMinutesSeconds(totalSeconds):
+    returnTime = ""
+    hrs = 0
+    mins = 0
+    secs = 0
+
+    if totalSeconds == 0:
+        return "0s"
+    if totalSeconds >= 3600:
+        hrs = totalSeconds//3600
+
+    mins = (totalSeconds - (3600 * hrs)) // 60
+    secs = (totalSeconds - (3600 * hrs)) % 60
+
+    if hrs > 0:
+        returnTime = str(hrs) + "h "
+    if mins > 0:
+        returnTime = str(returnTime) + str(mins) + "m "
+    if secs > 0:    
+        returnTime = str(returnTime) + str(secs) + "s "
+
+    return (returnTime.strip())
+        
 
 assert getHoursMinutesSeconds(30) == '30s'
 
