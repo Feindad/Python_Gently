bd4898263632aeb7205947464c53a8317ba2eac4
Feindad <Feindad@gmail.com>
2023-01-11 23:14:02 -0500

    Create PG12-get_smallest.py

---
+++ PG12-get_smallest.py
@@ -0,0 +1,13 @@
+# Python Gently  Ex-12 Smallest & Biggest
+
+def getSmallest([])
+
+assert getSmallest([1, 2, 3]) == 1
+
+assert getSmallest([3, 2, 1]) == 1
+
+assert getSmallest([28, 25, 42, 2, 28]) == 2
+
+assert getSmallest([1]) == 1
+
+assert getSmallest([]) == None
