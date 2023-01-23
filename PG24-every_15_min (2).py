b51d81545d806e72ff41573499eac7cfde9941ef
Feindad <Feindad@gmail.com>
2023-01-23 07:49:04 -0500

    Add files via upload

---
+++ PG24-every_15_min (2).py
@@ -0,0 +1,8 @@
+#Python Gently Ex24 Every 15 Minutes
+hrs = [12,1,2,3,4,5,6,7,8,9,10,11]
+qtrs = ["00","15","30","45"]
+
+for hr in hrs:
+    for qtr in qtrs:
+        print(str(hr)+':'+qtr)
+        
