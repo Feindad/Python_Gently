99e9f8184eed52ef662598dcc1d87b05e9ce41f1
Feindad <Feindad@gmail.com>
2023-01-28 18:46:37 -0500

    Update PG31-convert_to_string.py

--- PG31-convert_to_string.py
+++ PG31-convert_to_string.py
@@ -1,9 +1,30 @@
 #Python Gently Ex31 Convert integers to strings
 
 def convertIntToStr(number):
-    strnum = number * ""
+    strnum =''    
+    if number < 0:
+        strnum = ('-')
+    num = abs(number)
+    ex = 1
+    numlist = ['0','1','2','3','4','5','6','7','8','9']
+    while num >=1:
+     
+        while num >= 10:
+                ex = ex * 10
+                num = num/10
+                if round((num - int(num))*10) == 0:
+                    strnum = strnum + '0'
+        dig = int(num//1)
+        print(dig)
+        ret = numlist[dig]
+        strnum = strnum + (ret)
+        rem = num - dig
+        num =round(ex * rem)
+        ex = 1
     return strnum
-    
-for i in range(-10000, 10000):
 
+
+    
+for i in range(-99, 99):
+    print(i)
     assert convertIntToStr(i) == str(i)
