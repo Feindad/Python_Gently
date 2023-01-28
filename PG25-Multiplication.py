ae4060d0589077fbf4becb307f2b0cfe75864ea8
Feindad <Feindad@gmail.com>
2023-01-28 10:43:31 -0500

    Add files via upload

---
+++ PG25-Multiplication.py
@@ -0,0 +1,17 @@
+#Python Gently Ex25 Multiplication table
+spacer = " "
+for x in range(-1,11):
+    for y in range(1,11):
+        if x== 0:
+            if x == 0 and y == 1:
+                print(spacer*3, end='')
+            else:    
+                print('---', end="")
+        else:
+            if len(str(x*y))==1:
+                print(spacer, end="")
+            print(abs(x*y), end=" ")
+        if y == 1 and x>0:
+            print("|" + str(abs(x)), end=' ')
+        if y==10:
+            print()
